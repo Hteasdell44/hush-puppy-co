a966e9b675770e900d1e572f3b9cd56410a495c6
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-05-23 14:18:43 -0400

    Completed Homepage Design

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return(
+        <div id="footer-container">
+         
+            <ul>
+                <li>View Profile</li>
+                <li>Blog</li>
+                <li>About</li>
+                <li>Contact Us</li>
+            </ul>
+
+        </div>
+    );
+};
