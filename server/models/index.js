11615f3a190bd9328c3e204774db4b3cc59a093c
Hakim Teasdell <4keem@Hakims-Mac-mini.local>
2023-05-25 22:29:24 -0400

    Added DB Seeder

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require("./User");
-const StoreProduct = require("./Product");
+const Product = require("./Product");
 const BlogPost = require("./BlogPost");
 const Cart = require("./Cart");
 
-module.exports = { User, StoreProduct, BlogPost, Cart };
+module.exports = { User, Product, BlogPost, Cart };
---
+++ server/seeders/seed.js
@@ -0,0 +1,21 @@
+const db = require('../config/connection');
+const { User, Product, BlogPost, Cart } = require('../models');
+
+const userData = require('./data/userData.json');
+const productData = require('./data/productData.json');
+const blogData = require('./data/blogData.json');
+
+db.once('open', async () => {
+  await User.deleteMany({});
+  await Product.deleteMany({});
+  await Cart.deleteMany({});
+  await BlogPost.deleteMany({});
+
+  await User.insertMany(userData);
+  await Product.insertMany(productData);
+  await BlogPost.insertMany(blogData);
+
+
+  console.log('The Hush Puppy Company Database Has Been Seeded 🌱!');
+  process.exit(0);
+});
